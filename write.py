321213a9f530e9f063c8c9abb61558188bd4743b
nknsryo <nkns.ryo@gmail.com>
2022-02-03 11:02:56 +0900

    テキストファイルを作る_2

--- write.py
+++ write.py
@@ -1,11 +1,10 @@
 def main():
-    with open(file="users.txt", mode="w", encoding="utf-8") as f:
-        # 書き込み(上書き)　-> w  ,追加  -> a
-        f.write("Bob,79\n")
+    with open(file='users.txt', mode='w', encoding='utf-8') as f:
+        f.write('Bob,79\n')
+        f.write('Tom,59')
+        print('close前', f.closed)
 
-        f.write("Tom,59")
-    #     print("withの中:", f.closed)
-    # print("withの外:", f.closed)  ->
+    print('close後', f.closed)
 
 
 if __name__ == '__main__':
